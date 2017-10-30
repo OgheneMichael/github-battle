7227664e4e443d24e4f3e3465e79ccaab65555c2
OgheNerd <OgheNerd@OgheNerd.local>
2017-10-30 09:09:38 +0100

    children

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,8 @@
 var axios = require('axios');
 
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret" + sec;
+var id = "732c67f911ce1c6c6f0b";
+var sec = "06e489dcc9cda14d5f9cdf59672d79929730e68a";
+var params = "?client_id=" + id + "&client_secret=" + sec;
 
 function getProfile(username) {
   return axios.get('https://api.github.com/users/' + username + params)
